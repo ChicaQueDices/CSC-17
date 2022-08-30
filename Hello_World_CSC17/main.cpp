7c1d894e6b53993bdaaae0f176a69cfddb4b5e1d
ChicaQueDices <101829137+ChicaQueDices@users.noreply.github.com>
2022-08-29 21:55:45 -0700

    Hello_World_CSC17

---
+++ Hello_World_CSC17/main.cpp
@@ -0,0 +1,20 @@
+
+/* 
+ * File:   main.cpp
+ * Author: Venus Curtner
+ * Last Updated on: August 29th 9:48 PM
+ * Purpose: Hello World Program
+ */
+//System Libraries
+#include <iostream>  //Input/Output Library
+
+using namespace std;
+
+
+
+
+int main(int argc, char** argv) {
+
+    cout<<"Hello World!"; 
+    return 0;
+}
